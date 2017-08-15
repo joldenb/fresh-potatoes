19c9129a185571b55f288033fb3bb3cea43b5f80
Joe Oldenburg <joseph.oldenburg@gmail.com>
2017-08-15 17:23:07 -0600

    the endpoint can now retrieve all films with the correct genre

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
-const sqlite = require('sqlite'),
-      Sequelize = require('sequelize'),
-      request = require('request'),
-      express = require('express'),
-      app = express();
+const express = require('express');
+const Promise = require('bluebird');
+const db =  require('sqlite');
+const http = require('http-request');
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+const app = express();
+const port = process.env.PORT || 3000;
 
-// START SERVER
-Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
-  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
-// ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
-}
 
-module.exports = app;
+  try {
+
+  	// const limit = 10, // || params.limit;
+  	// 	offset = 1; // || params.offset;
+
+  	// first get the genre of the film being queried
+    db.get('SELECT genre_id FROM films WHERE id = ?', req.params.id)
+    .then(function(result){
+    	
+    	//set the genre id and get the next one
+    	if ( !result ) {
+    		throw new Error('film document not found');
+    	}
+    	
+    	return db.all('SELECT * FROM films WHERE genre_id = ?', result.genre_id)
+    })
+    .then(function(allResults){
+
+    	res.json({'result': allResults });
+
+
+    })
+    .catch(function(err){
+	    if ( err ){
+	    	res.json({'result' : 'there was an error with this request' });
+	    } else {
+				res.json({'result' : err });
+	    }
+    });
+
+
+
+  } catch (err) {
+    if ( err ){
+    	res.json({'result' : 'there was an error with this request' });
+    } else {
+			res.json({'result' : err });
+    }
+  }
+
+}
+ 
+ 
+Promise.resolve()
+  // First, try connect to the database 
+  .then(() => db.open('./db/database.db', { Promise }))
+  .catch(err => console.error(err.stack))
+  // Finally, launch Node.js app 
+  .finally(() => app.listen(port));
+
+
