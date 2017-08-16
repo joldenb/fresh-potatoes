d0cfbadd708a354a1c41260637cf52092343cad9
Joe Oldenburg <joseph.oldenburg@gmail.com>
2017-08-16 11:13:41 -0600

    updating to match the style guide

--- index.js
+++ index.js
@@ -8,115 +8,119 @@ const _ = require('lodash');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
 
 	try {
-			var allFilmResults = [];
-			var genre_name = "", genre_id;
-			var film_release_date = "";
-			var finalRecommendations = {
+			let allFilmResults = [];
+			let genreName = "", genreId;
+			let filmReleaseDate = "";
+			let finalRecommendations = {
 				"recommendations" : [],
 				"meta" : {}
 			};
 
-		if ( ( req.query.limit && !Number.isInteger(parseInt(req.query.limit)) ) || 
-			 ( req.query.offset && !Number.isInteger(parseInt(req.query.offset)) ) ||
-			 ( !req.params.id || (req.params.id && !Number.isInteger(parseInt(req.params.id)) ) ) ){
-			return res.status(422).json( { "message" : "Return an explicit error here" } )
+		//check if the parameters are valid
+		if ((req.query.limit && !Number.isInteger(parseInt(req.query.limit))) || 
+			 (req.query.offset && !Number.isInteger(parseInt(req.query.offset))) ||
+			 (!req.params.id || (req.params.id && !Number.isInteger(parseInt(req.params.id))))){
+			return res.status(422).json({"message" : "Return an explicit error here"});
 		}
 
 		const limit = req.query.limit || 10,
 			offset = req.query.offset || 0;
 
-			finalRecommendations["meta"]["limit"] = limit;
-			finalRecommendations["meta"]["offset"] = offset;
+		finalRecommendations["meta"]["limit"] = limit;
+		finalRecommendations["meta"]["offset"] = offset;
 
 		// first get the genre of the film being queried
 		db.get('SELECT genre_id, release_date FROM films WHERE id = ?', req.params.id)
-		.then(function(result){
+		.then( result => {
 			
 			//set the genre id and get the next one
-			if ( !result || !result.genre_id || !result.release_date) {
+			if (!result || !result.genre_id || !result.release_date) {
 				throw new Error('film document not found');
 			}
 
-			genre_id = result.genre_id;
+			genreId = result.genre_id;
+			filmReleaseDate = result.release_date;
 
-			film_release_date = result.release_date;
-
+			//get the name of the genre
 			return db.get('SELECT name FROM genres WHERE id = ?', result.genre_id);
 		})
-		.then(function(result){
+		.then( result => {
 
-			genre_name = result.name;
-
-			const release_date_ceiling = moment(film_release_date).add(15, 'y').format("YYYY-MM-DD");
-			const release_date_floor = moment(film_release_date).subtract(15, 'y').format("YYYY-MM-DD");
-
-			return db.all('SELECT * FROM films WHERE genre_id = ? AND release_date <= ? AND release_date >= ?', genre_id, release_date_ceiling, release_date_floor)
-		})
-		.then(function(allResults){
-
-			if ( !allResults || !allResults.length && allResults.length > 0) {
-				res.json({'all results': "no recommendations found" });
+			if (!result.name) {
+				throw new Error('no genre name');
 			}
 
-			allFilmResults = allResults;
+			// proceed to get all the films from the same genre within the date range
+			genreName = result.name;
 
-			var reviewQuery = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=";
+			const release_date_ceiling = moment(filmReleaseDate).add(15, 'y').format("YYYY-MM-DD");
+			const release_date_floor = moment(filmReleaseDate).subtract(15, 'y').format("YYYY-MM-DD");
+
+			return db.all('SELECT * FROM films WHERE genre_id = ? AND release_date <= ? AND release_date >= ?', genreId, release_date_ceiling, release_date_floor)
+		})
+		.then( allResults => {
+
+			if (!allResults || !allResults.length && allResults.length > 0) {
+				throw new Error('no valid reviews');
+			}
+
+			// get all the reviews for the films within range
+			allFilmResults = allResults;
+			let reviewQuery = "http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=";
 
 			_.forEach(allResults, function( singleResult ) {
 				reviewQuery = reviewQuery + singleResult.id + ","
 			});
 
-			var options = {
-					uri: reviewQuery,
-					headers: {
-							'User-Agent': 'Request-Promise'
-					},
-					json: true // Automatically parses the JSON string in the response 
+			let options = {
+				uri: reviewQuery,
+				headers: {
+						'User-Agent': 'Request-Promise'
+				},
+				json: true
 			};    	
 
 			return http.get(options)
 		})
-		.then(function(results){
+		.then( results => {
 
-				var reviewResults = results;
+				let reviewResults = results;
 				
 				//filter out the movies with less than 5 reviews
-				reviewResults = _.filter(reviewResults, function(oneFilm) { return oneFilm.reviews.length >= 5; } );
+				reviewResults = _.filter(reviewResults, oneFilm => { return oneFilm.reviews.length >= 5;});
 
 				//of the films that are left, filter out the ones that dont have an average rating of at least 4
-				_.forEach(reviewResults, function(oneFilm) {
+				_.forEach(reviewResults, oneFilm => {
 
-					var ratings_total = 0;
+					let ratingsTotal = 0;
 					_.forEach(oneFilm.reviews, function(oneReview, index){
-						ratings_total = ratings_total + oneReview.rating;
+						ratingsTotal = ratingsTotal + oneReview.rating;
 					});
 
-					const average_rating = Math.round( ( ratings_total / oneFilm.reviews.length) * 100 ) / 100;
+					const average_rating = Math.round( ( ratingsTotal / oneFilm.reviews.length) * 100 ) / 100;
 					
 					//if the average rating is above 4.0, add it to the list of final recommendations
 					if ( average_rating >= 4.0 ){
 
-						var film_name = "", release_date = "";
-						_.forEach(allFilmResults, function(film) {
+						let filmName = "", releaseDate = "";
+						_.forEach(allFilmResults, film => {
 							if ( film.id == oneFilm.film_id ){
-								film_name = film.title;
-								release_date = film.release_date;
+								filmName = film.title;
+								releaseDate = film.release_date;
 							}
 						})
 
-						var new_film_recommendation = {};
+						let new_film_recommendation = {};
 						new_film_recommendation["id"] = oneFilm.film_id;
-						new_film_recommendation["title"] = film_name;
-						new_film_recommendation["releaseDate"] = release_date;
-						new_film_recommendation["genre"] = genre_name;
+						new_film_recommendation["title"] = filmName;
+						new_film_recommendation["releaseDate"] = releaseDate;
+						new_film_recommendation["genre"] = genreName;
 						new_film_recommendation["averageRating"] = average_rating;
 						new_film_recommendation["reviews"] = oneFilm.reviews.length;
 						finalRecommendations["recommendations"].push(new_film_recommendation);
@@ -126,15 +130,16 @@ function getFilmRecommendations(req, res) {
 
 
 				//finally, sort by id, limit, and offset the results as needed
-				var recommendationArray = finalRecommendations["recommendations"];
+				let recommendationArray = finalRecommendations["recommendations"];
 				recommendationArray = _.sortBy(recommendationArray, 'id');
 				recommendationArray = recommendationArray.slice(offset, offset + limit);
 				finalRecommendations["recommendations"] = recommendationArray;
 
+				//return the formatted list of recommendations
 				return res.json(finalRecommendations);
 
 		})
-		.catch(function(err){
+		.catch( err => {
 			if ( !err ){
 				res.json({'result' : 'there was an error with this request' });
 			} else {
@@ -150,10 +155,8 @@ function getFilmRecommendations(req, res) {
 	}
 
 }
- 
-	
 
-app.get('*', function(req, res){
+app.get('*', (req, res) => {
 	return res.status(404).json( { "message" : "Return an explicit error here" } )
 });
 	
@@ -167,4 +170,3 @@ Promise.resolve()
 
 
 module.exports = app;
-
