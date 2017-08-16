0219be2c20c10c93b60ff5a049353801a2c1e0c6
Joe Oldenburg <joseph.oldenburg@gmail.com>
2017-08-15 18:12:12 -0600

    adding the release date constraint to recommendations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Promise = require('bluebird');
 const db =  require('sqlite');
 const http = require('http-request');
+const moment = require('moment');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,34 +19,35 @@ function getFilmRecommendations(req, res) {
   	// 	offset = 1; // || params.offset;
 
   	// first get the genre of the film being queried
-    db.get('SELECT genre_id FROM films WHERE id = ?', req.params.id)
+    db.get('SELECT genre_id, release_date FROM films WHERE id = ?', req.params.id)
     .then(function(result){
     	
     	//set the genre id and get the next one
-    	if ( !result ) {
+    	if ( !result || !result.genre_id || !result.release_date) {
     		throw new Error('film document not found');
     	}
-    	
-    	return db.all('SELECT * FROM films WHERE genre_id = ?', result.genre_id)
+
+    	console.log("the release date is" + moment(result.release_date).format("YYYY-MM-DD") );
+
+    	const release_date_ceiling = moment(result.release_date).add(15, 'y').format("YYYY-MM-DD");
+    	const release_date_floor = moment(result.release_date).subtract(15, 'y').format("YYYY-MM-DD");
+
+    	return db.all('SELECT * FROM films WHERE genre_id = ? AND release_date <= ? AND release_date >= ?', result.genre_id, release_date_ceiling, release_date_floor)
     })
     .then(function(allResults){
 
-    	res.json({'result': allResults });
-
+    	res.json({'all results': allResults });
 
     })
     .catch(function(err){
-	    if ( err ){
+	    if ( !err ){
 	    	res.json({'result' : 'there was an error with this request' });
 	    } else {
 				res.json({'result' : err });
 	    }
     });
-
-
-
   } catch (err) {
-    if ( err ){
+    if ( !err ){
     	res.json({'result' : 'there was an error with this request' });
     } else {
 			res.json({'result' : err });
